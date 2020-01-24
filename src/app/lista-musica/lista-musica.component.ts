4487a5783ab34a98a4b83978f8c4624b0d65a844
Gabriel Girardi <ggirardii@hotmail.com>
2020-01-23 23:23:26 -0300

    adicionado botão para deletar da lista

--- src/app/lista-musica/lista-musica.component.ts
+++ src/app/lista-musica/lista-musica.component.ts
@@ -10,9 +10,11 @@ export class ListaMusicaComponent implements OnInit {
 
   @Input() titulo : string;
   @Input() musicas : Array<Musica> = new Array<Musica>();
-
   constructor() { }
   ngOnInit(): void {
   }
 
+  removerMusica(musica : Musica) : void {
+    this.musicas = this.musicas.filter(x => x.id != musica.id);
+  }
 }
